3dbfd0d7888437694bfcc7f3907d8f5460444a81
fanxiaotuGod <fhc991115@gmail.com>
2025-07-17 18:10:08 -0700

    correct logic in server.js

--- src/server.js
+++ src/server.js
@@ -6,10 +6,7 @@ const exampleRoutes = require('./routes/example/hello');
 const app = express();
 const port = process.env.TLEF_CREATE_PORT || 8090;
 
-const isProduction = process.env.NODE_ENV === 'production';
-const staticDir = isProduction
-  ? path.join(__dirname, '../dist')
-  : path.join(__dirname, '../public');
+const staticDir = path.join(__dirname, '../dist');
 
 app.use(express.static(staticDir));
 
@@ -17,11 +14,10 @@ app.use(express.static(staticDir));
 app.use('/api/example', exampleRoutes);
 
 // Serve index.html for all routes (SPA fallback)
-if (isProduction) {
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(staticDir, 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(staticDir, 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
