77f442ab1468d9ab6dff96537e4573ade5dc592a
fanxiaotuGod <fhc991115@gmail.com>
2025-08-18 15:42:24 -0700

    fix bugs

--- src/routes/example/hello.js
+++ src/routes/example/hello.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
 router.get('/hello', (req, res) => {
   res.json({ message: 'Hello from the server!' });
 });
 
-module.exports = router;
+export default router;
